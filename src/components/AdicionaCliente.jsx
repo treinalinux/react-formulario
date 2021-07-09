f74cecb5bc770ef85f97c4243fd1c1970766d510
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-08 22:18:30 -0300

    feat: format project with prettier

--- src/components/AdicionaCliente.jsx
+++ src/components/AdicionaCliente.jsx
@@ -1,55 +1,59 @@
-import React from 'react';
-import { Formik } from 'formik'
+import React from "react";
+import { Formik } from "formik";
 
 const AdicionaCliente = () => {
-  return (
-    <>
-      <h1>Cadastro de Clientes</h1>
+    return (
+        <>
+            <h1>Cadastro de Clientes</h1>
 
-    <Formik
-    initialValues={{ nome: 'Seu Nome', email: 'seu@mail.com', nascimento: ''}}
-    onSubmit={(values) => {
-      alert(JSON.stringify(values))
-    }}
-    >
-        {(props) => (
-          <form onSubmit={props.handleSubmit} noValidate>
-            <div className="form-group">
-              <label htmlFor="nome">Nome</label>
-              <input
-                type="text"
-                id="nome"
-                name="nome"
-                value={props.values.nome}
-                onChange={props.handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={props.values.email}
-                onChange={props.handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="date">Data de Nascimento</label>
-              <input
-                type="date"
-                id="nascimento"
-                name="nascimento"
-                value={props.values.nascimento}
-                onChange={props.handleChange}
-              />
-            </div>
-            <button type="submit">Adicionar</button>
-          </form>
-        )}
-      </Formik>
-    </>
-  );
+            <Formik
+                initialValues={{
+                    nome: "Seu Nome",
+                    email: "seu@mail.com",
+                    nascimento: "03/10/1984",
+                }}
+                onSubmit={(values) => {
+                    alert(JSON.stringify(values));
+                }}
+            >
+                {(props) => (
+                    <form onSubmit={props.handleSubmit} noValidate>
+                        <div className="form-group">
+                            <label htmlFor="nome">Nome</label>
+                            <input
+                                type="text"
+                                id="nome"
+                                name="nome"
+                                value={props.values.nome}
+                                onChange={props.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={props.values.email}
+                                onChange={props.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="date">Data de Nascimento</label>
+                            <input
+                                type="date"
+                                id="nascimento"
+                                name="nascimento"
+                                value={props.values.nascimento}
+                                onChange={props.handleChange}
+                            />
+                        </div>
+                        <button type="submit">Adicionar</button>
+                    </form>
+                )}
+            </Formik>
+        </>
+    );
 };
 
 export default AdicionaCliente;
