e5cf5c20d97b28cc90b907a7adb2aa6aca290642
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-08 23:50:46 -0300

    feat: created validates simple on form

--- src/components/AdicionaCliente.jsx
+++ src/components/AdicionaCliente.jsx
@@ -8,9 +8,28 @@ const AdicionaCliente = () => {
 
             <Formik
                 initialValues={{
-                    nome: "Seu Nome",
-                    email: "seu@mail.com",
-                    nascimento: "03/10/1984",
+                    nome: "",
+                    email: "",
+                    nascimento: "",
+                }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.nome) {
+                        errors.nome = "O nome é obrigatório";
+                    }
+                    if (!values.email) {
+                        errors.email = "O email é obrigatório";
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                            values.email
+                        )
+                    ) {
+                        errors.email = "O email é inválido";
+                    }
+                    if (!values.nascimento) {
+                        errors.nascimento = "A data nascimento é obrigatório";
+                    }
+                    return errors;
                 }}
                 onSubmit={(values) => {
                     alert(JSON.stringify(values));
@@ -26,7 +45,20 @@ const AdicionaCliente = () => {
                                 name="nome"
                                 value={props.values.nome}
                                 onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                className={
+                                    props.errors.nome && props.touched.nome ? (
+                                        <div className="is_invalid"></div>
+                                    ) : (
+                                        ""
+                                    )
+                                }
                             />
+                            {props.errors.nome && props.touched.nome ? (
+                                <div className="invalid-feedback">
+                                    {props.errors.nome}
+                                </div>
+                            ) : null}
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
@@ -36,7 +68,21 @@ const AdicionaCliente = () => {
                                 name="email"
                                 value={props.values.email}
                                 onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                className={
+                                    props.errors.email &&
+                                    props.touched.email ? (
+                                        <div className="is_invalid"></div>
+                                    ) : (
+                                        ""
+                                    )
+                                }
                             />
+                            {props.errors.email && props.touched.email ? (
+                                <div className="invalid-feedback">
+                                    {props.errors.email}
+                                </div>
+                            ) : null}
                         </div>
                         <div className="form-group">
                             <label htmlFor="date">Data de Nascimento</label>
@@ -46,7 +92,22 @@ const AdicionaCliente = () => {
                                 name="nascimento"
                                 value={props.values.nascimento}
                                 onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                className={
+                                    props.errors.nascimento &&
+                                    props.touched.nascimento ? (
+                                        <div className="is_invalid"></div>
+                                    ) : (
+                                        ""
+                                    )
+                                }
                             />
+                            {props.errors.nascimento &&
+                            props.touched.nascimento ? (
+                                <div className="invalid-feedback">
+                                    {props.errors.nascimento}
+                                </div>
+                            ) : null}
                         </div>
                         <button type="submit">Adicionar</button>
                     </form>
